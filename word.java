179eea90839762d14d2aa987f99c625c86828f61
eden7777 <75039749+eden7777@users.noreply.github.com>
2020-11-26 19:57:34 +0200

    Create word.java

---
+++ word.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+     /*   System.out.println("Hello world!");
+        System.out.println("please enter your name:");
+
+        Scanner s = new Scanner(System.in);// chiese
+        String name = s.nextLine();
+        System.out.println("Hello");
+        System.out.println(name);
+*/
+
+      byte player_level = 1;
+        float today = 23.11f;
+        double varibel = 8.8876;
+        boolean edenY = true;
+        boolean edenL = false;
+        System.out.println("")
+
+     /*   Targil:
+        1. create an int variable which will have the value of: 785 * 231
+        2. create a float variable with the value of 4.3 * 7.8
+        3. create char named "lettx" with 'x'
+        4. create char named "letty" with 'y
+        5. create in one line short xs,ys,zs in this one line put in xs = 1 in ys = 2 in zs = 3
+                *etgar: then in the next line put in xs the sum of ys and zs
+*/
+
+        public static void main(String[] args)}
+    {
+    int room = 785 * 231;
+    float width = 4.3f;
+    float length = 7.8f;
+    char lettx = 'x';
+    char letty = 'y';
+    short xs,ys, zs = xs = 1, ys = 2, zs = 3;
+
+
+}
+
+
+
+
+{
